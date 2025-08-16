c0147646f5f1559bcd003ce95b632b5b6eb92918
kevinsantiago-stack <larrotacuervokevinsantiago@gmail.com>
2025-08-16 00:38:27 -0500

    Create modulo4.py

---
+++ modulo4.py
@@ -0,0 +1,87 @@
+# 1 ejercio operadores aritmeticos 
+
+a = 10
+b = 3
+
+también = a + b
+imprimir("Suma (a + b):", suma)
+
+resta = a - b
+print("Resto (ab):", resto)
+
+multiplicación = a * b
+print("Multiplicación (a* b):", multiplicacion)
+
+División = A / B
+print("División (a/b):", división)
+
+# 2 ejercicio operadores logicos
+
+edad int(input("Ingresa tu edad: "))
+
+#Tiene permiso de los padres
+permiso = False
+
+# Usamos AND: debe tener al menos 18 años y permiso (aunque aqui
+si edad >= 18 y permiso:
+  print("Puede entrar solo a la película para adultos.")
+
+elif edad >= 18 o permiso:
+  print("Puede entrar con permiso.")
+# Usamos NOT: si no tiene permiso
+elif not permiso:
+  print("No puede entrar, necesita permiso.")
+
+# 3 ejercicio operadores de cadena 
+
+nombre = "kevin"
+saludo = "HOLA, + nombre + "!"
+print(saludo)
+
+risa "JA" * 5
+print("Risa:", risa)
+
+frase "la" + " " + "vida" + + "genial" + " " + "es" + " " + "genial"
+print(frase)
+
+# 4 ejercicio operadores rqcionales
+
+a = int(input("Ingresa el primer número: "))
+b = int(input("Ingresa el segundo número: "))
+
+print("¿a es igual a b?", a == b)
+print("¿a es diferente de b?:", a != b)
+print("¿a es mayor que b?:", a > b)
+print("¿a es menor que b?:", a < b)
+print("¿a es mayor o igual que b?", a >= b)
+print("¿a es menor o igual que b?", a <= b)
+
+# 5 ejercicio operadores de inclusion
+
+frutas = ["manzana", "pera", "uva", "banana"]
+
+fruta input("Escribe el nombre de una fruta: ").bajar()
+
+Si fruta en frutas:
+    print("¡Sí, la fruta está en la lista!")
+de lo contrario:
+    print("No... esa fruta no está en la lista.")
+
+# 6 ejercicio operadores de asignacion
+
+número = 20
+print("Número inicial:", numero)
+
+número += 10
+print("Después de sumar 10:", número)
+
+número - 5
+print("Después de restar 5:", número)
+
+número *= 2
+print("Después de multiplicar por 2:", numero)
+
+número /= 5
+print("Después de dividir entre 5:", número)
+
+
