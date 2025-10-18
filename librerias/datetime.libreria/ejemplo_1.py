9bb56a915b476993bfb137cf324a70092ca4b14a
kevinsantiago-stack <larrotacuervokevinsantiago@gmail.com>
2025-10-17 19:12:05 -0500

    Delete librerias directory

--- librerias/datetime.libreria/ejemplo_1.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-
-hoy = date.today()
-print("Fecha actual:", hoy)
-
-cumple = date(2000, 5, 15)
-print("Cumpleaños:", cumple)
--- librerias/request.libreria/ejemplo_1.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.get("https://randomuser.me/api/")
-usuario = r.json()
-print (usuario)
