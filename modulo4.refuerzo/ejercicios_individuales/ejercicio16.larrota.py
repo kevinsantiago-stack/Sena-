527fc13beeaa039bc5eabd757ffbd29eacb7873c
kevinsantiago-stack <larrotacuervokevinsantiago@gmail.com>
2025-11-28 16:00:22 -0500

    Update fields in PostSerializer to include 'larrota'

--- modulo4.refuerzo/ejercicios_individuales/ejercicio16.larrota.py
+++ modulo4.refuerzo/ejercicios_individuales/ejercicio16.larrota.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id','titulo','cuerpo','autor','creado']
+        fields = ['id','titulo','larrota','autor','creado']
